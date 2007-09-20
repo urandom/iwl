498c902f7100c83ebeb87f282b8ed0a9d50603d5
vkojouharov <vkojouharov@d3eaf211-ee32-0410-a207-cdbac743c15d>
2007-09-20 16:06:30 +0000

    more work on the resizer

--- share/jscript/resizer.js
+++ share/jscript/resizer.js
@@ -1,138 +1,149 @@
 // vim: set autoindent shiftwidth=2 tabstop=8:
 //
 var Resizer = Class.create((function() {
-  var handles = {};
-
-  var eventMouseUp   = Prototype.emptyFunction;
-  var eventMouseDown = Prototype.emptyFunction;
-  var eventMouseMove = Prototype.emptyFunction;
-  var eventSelect    = Prototype.emptyFunction;
-
-  var prevPointerX = 0;
-  var prevPointerY = 0;
-
-  var resize = false;
-  var resizeType;
-  var elementPosition = {w: 0, h: 0, x: 0, y: 0};
-  var elementStyle = {};
-
   function setup() {
-    eventMouseUp   = mouseUp.bindAsEventListener(this);
-    eventMouseDown = mouseDown.bindAsEventListener(this);
-    eventMouseMove = mouseMove.bindAsEventListener(this);
-    eventSelect    = selectElement.bindAsEventListener(this);
+    var eventMouseUp   = mouseUp.bindAsEventListener(this);
+    var eventMouseDown = mouseDown.bindAsEventListener(this);
+    var eventMouseMove = mouseMove.bindAsEventListener(this);
+    var eventSelect    = selectElement.bindAsEventListener(this);
+
+    this.prevPointerX = 0;
+    this.prevPointerY = 0;
+
+    this.resize = false;
+    this.resizeType;
+
+    this.elementPosition = {w: 0, h: 0, x: 0, y: 0};
+    this.elementStyle    = {};
 
     if (this.element.getStyle('position') == 'static') {
       this.element.style.position = 'relative';
-      elementStyle.position = 'static';
+      this.elementStyle.position = 'static';
     }
     this.element.resizer = this;
+    this.handlers = {};
 
-    createHandles.call(this);
-    Event.observe(this.element, "mousedown", eventSelect);
-    Event.observe(document,     "mouseup"  , eventMouseUp);
-    Event.observe(document,     "mousemove", eventMouseMove);
+    createHandles.call(this, eventMouseDown);
+    var ok = true;
+
+    this.options.togglers.each(function(toggler) {
+        toggler = $(toggler);
+        if (!toggler)
+          return;
+        toggler.observe("mousedown", eventSelect);
+        ok = false;
+    });
+    Event.observe(document, "mouseup"  , eventMouseUp);
+    Event.observe(document, "mousemove", eventMouseMove);
+    if (ok)
+      Event.observe(this.element, "mousedown", eventSelect);
   }
 
-  function createHandles() {
+  function createHandles(observer) {
     if (!this.options.vertical && !this.options.horizontal) {
-      ['tl', 'tr', 'bl', 'br'].each(createHandle.bind(this));
+      ['tl', 'tr', 'bl', 'br'].each(function($_) {
+          createHandle($_, observer);
+      }.bind(this));
     } 
     if (this.options.vertical || !this.options.horizontal) {
-      ['t', 'b'].each(createHandle.bind(this));
+      ['t', 'b'].each(function($_) {
+          createHandle($_, observer);
+      }.bind(this));
     }
     if (!this.options.vertical) {
-      ['l', 'r'].each(createHandle.bind(this));
+      ['l', 'r'].each(function($_) {
+          createHandle($_, observer);
+      }.bind(this));
     }
   }
 
   function destroyHandles() {
-    for (var h in handles)
+    for (var h in this.handlers)
       h.remove();
-    handles = {};
+    this.handlers = {};
   }
 
-  function createHandle(type) {
-    if (handles[type]) return;
+  function createHandle(type, observer) {
+    if (this.handlers[type]) return;
     var handle = new Element('div', {
         className: this.options.className + ' '
           + this.options.className + '_' + type,
         style: 'visibility: hidden;'
     });
-    handles[type] = this.element.appendChild(handle);
+    this.handlers[type] = this.element.appendChild(handle);
     handle.type = type;
-    handle.observe('mousedown', eventMouseDown);
+    handle.observe('mousedown', observer);
   }
   
   function toggleHandles() {
-    for (var h in handles) {
-      var v = handles[h].getStyle('visibility');
-      handles[h].setStyle({visibility: v == 'hidden' ? '' : 'hidden'});
+    for (var h in this.handlers) {
+      var v = this.handlers[h].getStyle('visibility');
+      this.handlers[h].setStyle({visibility: v == 'hidden' ? '' : 'hidden'});
     }
   }
 
   function mouseUp(event) {
-    resize = false;
-    prevPointerX = 0;
-    prevPointerY = 0;
+    this.resize = false;
+    this.prevPointerX = 0;
+    this.prevPointerY = 0;
   }
 
   function mouseDown(event) {
     if (this.options.resizeStartCallback
       && !(this.options.resizeStartCallback(this.element, event)))
       return;
-    resize = true;
-    resizeType = event.element().type || '';
+    this.resize = true;
+    this.resizeType = event.element().type || '';
   }
 
   function mouseMove(event) {
-    if (!resize) return;
+    if (!this.resize) return;
 
     var pX = event.pointerX();
     var pY = event.pointerY();
-    var dX = pX - (prevPointerX || pX);
-    var dY = pY - (prevPointerY || pY);
+    var dX = pX - (this.prevPointerX || pX);
+    var dY = pY - (this.prevPointerY || pY);
     var ok = false;
 
-    prevPointerX = pX;
-    prevPointerY = pY;
+    this.prevPointerX = pX;
+    this.prevPointerY = pY;
 
-    if (resizeType.indexOf('t') >= 0) {
-      elementPosition.y += dY;
-      elementPosition.h -= dY;
+    if (this.resizeType.indexOf('t') >= 0) {
+      this.elementPosition.y += dY;
+      this.elementPosition.h -= dY;
       ok = true;
-    } else if (resizeType.indexOf('b') >= 0) {
-      elementPosition.h += dY;
+    } else if (this.resizeType.indexOf('b') >= 0) {
+      this.elementPosition.h += dY;
       ok = true;
     }
 
-    if (resizeType.indexOf('l') >= 0) {
-      elementPosition.x += dX;
-      elementPosition.w -= dX;
+    if (this.resizeType.indexOf('l') >= 0) {
+      this.elementPosition.x += dX;
+      this.elementPosition.w -= dX;
       ok = true;
-    } else if (resizeType.indexOf('r') >= 0) {
-      elementPosition.w += dX;
+    } else if (this.resizeType.indexOf('r') >= 0) {
+      this.elementPosition.w += dX;
       ok = true;
     }
 
     if (!ok) return;
-    if (elementPosition.w > this.options.maxWidth)
-      elementPosition.w = this.options.maxWidth;
-    else if (elementPosition.w < this.options.minWidth)
-      elementPosition.w = this.options.minWidth;
+    if (this.elementPosition.w > this.options.maxWidth)
+      this.elementPosition.w = this.options.maxWidth;
+    else if (this.elementPosition.w < this.options.minWidth)
+      this.elementPosition.w = this.options.minWidth;
 
-    if (elementPosition.h > this.options.maxHeight)
-      elementPosition.h = this.options.maxHeight;
-    else if (elementPosition.h < this.options.minHeight)
-      elementPosition.h = this.options.minHeight;
+    if (this.elementPosition.h > this.options.maxHeight)
+      this.elementPosition.h = this.options.maxHeight;
+    else if (this.elementPosition.h < this.options.minHeight)
+      this.elementPosition.h = this.options.minHeight;
 
-    this.element.style.width  = elementPosition.w + 'px';
-    this.element.style.height = elementPosition.h + 'px';
-    this.element.style.left   = elementPosition.x + 'px';
-    this.element.style.top    = elementPosition.y + 'px';
+    this.element.style.width  = this.elementPosition.w + 'px';
+    this.element.style.height = this.elementPosition.h + 'px';
+    this.element.style.left   = this.elementPosition.x + 'px';
+    this.element.style.top    = this.elementPosition.y + 'px';
 
     if (this.options.resizeCallback)
-      this.options.resizeCallback(this.element, event, elementPosition);
+      this.options.resizeCallback(this.element, event, this.elementPosition);
   }
 
   function selectElement(event) {
@@ -142,12 +153,12 @@ var Resizer = Class.create((function() {
     toggleHandles();
 
     var dimensions = this.element.getDimensions();
-    elementPosition.w = dimensions.width;
-    elementPosition.h = dimensions.height;
-    elementPosition.x = parseInt(this.element.style.left);
-    elementPosition.y = parseInt(this.element.style.top);
-    if (isNaN(elementPosition.x)) elementPosition.x = 0;
-    if (isNaN(elementPosition.y)) elementPosition.y = 0;
+    this.elementPosition.w = dimensions.width;
+    this.elementPosition.h = dimensions.height;
+    this.elementPosition.x = parseInt(this.element.style.left);
+    this.elementPosition.y = parseInt(this.element.style.top);
+    if (isNaN(this.elementPosition.x)) this.elementPosition.x = 0;
+    if (isNaN(this.elementPosition.y)) this.elementPosition.y = 0;
   }
 
 
@@ -162,6 +173,7 @@ var Resizer = Class.create((function() {
         maxWidth: 9999,
         minWidth: 10,
         className: 'resizer_handle',
+        togglers: [],
         resizeStartCallback: function() { return true; },
         resizeCallback: Prototype.emptyFunction,
       }, arguments[1] || {});
@@ -174,7 +186,7 @@ var Resizer = Class.create((function() {
 
     destroy: function() {
       destroyHandles.call(this);
-      this.element.setStyle(elementStyle);
+      this.element.setStyle(this.elementStyle);
       this.element.resizer = undefined;
     }
   }
